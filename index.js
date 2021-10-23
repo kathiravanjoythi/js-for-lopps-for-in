b4ef8a55e2a3c0367393cd9fe372255dc85dd020
kathiravanjoythi <kathiravanjoythi@gmail.com>
2021-10-23 20:41:31 +0530

    for of, while

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
  // forlopps
  for (let f = 1; f <=7; f++) {
-  console.log(f)
+  // console.log(f)
   if (f==5){
   break;
   }
@@ -19,5 +19,25 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
     }
   let car = {color: 'black', price: 500000}
  for(let k in car) {
- console.log(k , car[k])
+//  console.log(k , car[k])
  }
+
+//  for of 
+// string arrays
+let marks = [10, 20, 30, 40]
+for(let mark of marks) {
+  // console.log(mark)
+  if(mark == 30) {
+    break;
+  }
+}
+
+let lname = 'kumaran'
+for(let nm of lname) {
+  // console.log(nm)
+  if(nm == 'r') {
+    break;
+  }
+}
+
+
